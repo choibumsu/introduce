83364e7118b1f24d06405f4b3f14a46c46b1ff23
choibumsu <bumsu0211@gmail.com>
2020-09-20 23:59:08 +0900

    애니메이션 보강

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,10 +2,10 @@
   <div id="main-page">
     <section class="main">
       <div class="title-wrapper">
-        <h1 class="title">
+        <h1 class="title" :class="{ 'before-load': !isLoad }">
           <a :href="urls.github" target="blank">{{ name.eng }}</a>
         </h1>
-        <p class="description">Frontend Developer</p>
+        <p class="description" :class="{ 'before-load': !isLoad }">Frontend Developer</p>
       </div>
     </section>
   </div>
@@ -15,6 +15,7 @@
 import { Vue } from 'vue-class-component'
 
 export default class MainPage extends Vue {
+  isLoad = false
   name = {
     kor: '최범수',
     eng: 'Choi Bumsu',
@@ -25,9 +26,8 @@ export default class MainPage extends Vue {
 
   mounted() {
     setTimeout(() => {
-      const $title = this.$el.querySelector('.title')
-      $title.style.top = '-10rem'
-    }, 100)
+      this.isLoad = true
+    }, 50)
   }
 }
 </script>
@@ -46,6 +46,8 @@ export default class MainPage extends Vue {
       background-color: $color-gray-50;
 
       .title-wrapper {
+        $title-duration: 0.5s;
+
         display: flex;
         justify-content: center;
         flex-direction: column;
@@ -53,27 +55,40 @@ export default class MainPage extends Vue {
         width: 100%;
 
         .title {
-          transition: 0.5s;
+          transition: ease-in-out $title-duration;
           position: absolute;
-          top: 9rem;
+          top: -11rem;
+          opacity: 1;
           width: 100%;
-          color: $color-gray-900;
+
           font: {
             size: 8rem;
             weight: 400;
           }
+          color: $color-gray-900;
           text-align: center;
           line-height: 9rem;
           margin-bottom: 2rem;
+
+          &.before-load {
+            top: 8rem;
+            opacity: 0;
+          }
         }
 
         .description {
-          color: $color-main;
+          transition: ease 1.2s;
+          transition-delay: $title-duration;
           font: {
             size: 3rem;
             weight: 300;
           }
+          color: $color-main;
           text-align: center;
+
+          &.before-load {
+            opacity: 0;
+          }
         }
       }
     }
