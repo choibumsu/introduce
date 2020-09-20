411f7e1243bba75e1b77373939cfa6d8bf0aff7b
choibumsu <bumsu0211@gmail.com>
2020-09-20 17:00:00 +0900

    create vue3.0 cli project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import './assets/scss/package';
+@import './assets/scss/reset';
+</style>
