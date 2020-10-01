b78ec1c570e6d9664850a8328a55eebbb0783e62
choibumsu <bumsu0211@gmail.com>
2020-10-01 22:21:29 +0900

    Vuex에 전역 변수 저장

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,21 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {},
+  state: {
+    profile: {
+      name: {
+        kor: '최범수',
+        eng: 'Choi Bumsu',
+      },
+      email: 'bumsu0211@gmail.com',
+      phone: '010-7408-0064',
+    },
+    urls: {
+      github: 'https://github.com/choibumsu',
+      instagram: 'https://www.instagram.com/bbamsu211/?hl=ko',
+      facebook: 'https://www.facebook.com/bum0211',
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
