7176e3156c3f81bbc0da4fb198f54f5e881c7a63
choibumsu <bumsu0211@gmail.com>
2020-10-01 21:58:08 +0900

    아이콘 저작권 표시

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <router-view />
+  <div class="dp-none">
+    Icons made by
+    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from
+    <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+  </div>
 </template>
 
 <style lang="scss">
