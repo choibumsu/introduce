da4af07e5c9ec80d69a19a6b622c723510fc413c
choibumsu <bumsu0211@gmail.com>
2020-10-04 23:19:47 +0900

    free 공간 작성

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import MainPage from '@/views/MainPage.vue'
+import FreePage from '@/views/FreePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'MainPage',
     component: MainPage,
+  },
+  {
+    path: '/free',
+    name: 'FreePage',
+    component: FreePage,
   },
 ]
 
---
+++ src/views/FreePage.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="free-page" class="">
+    <div class="page p-50">
+      <div class="flex gap-10 p-10">
+        <div class="w-120 h-120 bg-orange box"></div>
+        <div class="w-120 h-120 bg-green circle"></div>
+        <img class="w-120 h-120" src="../assets/img/instagram.svg" />
+      </div>
+      <div class="flex center w-120 h-120 p-10 b-black-1 m-10">박스 크기</div>
+    </div>
+    <div class="page p-20">
+      <div class="w-800 h-300 bg-green"></div>
+      <div class="flex">
+        <div class="w-550 h-400 bg-orange"></div>
+        <div class="w-250 h-400 bg-orangered"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="scss">
+.page {
+  height: 100vh;
+}
+
+.flex {
+  display: flex;
+
+  &.center {
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+.gap- {
+  &10 {
+    gap: 10px;
+  }
+}
+
+.p- {
+  &10 {
+    padding: 10px;
+  }
+
+  &20 {
+    padding: 20px;
+  }
+
+  &50 {
+    padding: 50px;
+  }
+}
+
+.b- {
+  &black- {
+    &1 {
+      border: 1px solid black;
+    }
+  }
+}
+
+.m- {
+  &10 {
+    margin: 10px;
+  }
+}
+
+.w- {
+  &120 {
+    width: 120px;
+  }
+
+  &250 {
+    width: 250px;
+  }
+
+  &550 {
+    width: 550px;
+  }
+
+  &800 {
+    width: 800px;
+  }
+}
+
+.h- {
+  &120 {
+    height: 120px;
+  }
+
+  &300 {
+    height: 300px;
+  }
+
+  &400 {
+    height: 400px;
+  }
+}
+
+.bg- {
+  &orange {
+    background-color: orange;
+  }
+
+  &green {
+    background-color: green;
+  }
+
+  &orangered {
+    background-color: orangered;
+  }
+}
+
+.circle {
+  border-radius: 50%;
+}
+</style>
